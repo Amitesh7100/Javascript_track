a698d6695982175a4992d75a9d593f4c14bd953d
Amitesh7100 <110113872+Amitesh7100@users.noreply.github.com>
2024-12-09 17:30:17 +0000

    datatypes

---
+++ learn/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello")  // we are using nodejs, not browser
+//code readability should be high
+
+
+ console.logf("hitesh")
+
+ let name="amitesh"
+ let age=64
+ let isLoggedIn =false
+ let state
+
+//number => 2 to power 53
+//bigint
+//string =. " "
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
---
+++ learn/variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail="asr@google.com"
+var accountPassword="123456"
+accountCity="ujjain"
+let accountState
+//accountId=2
+
+accountEmail="asfe.com"
+accountPassword="321625"
+accountCity="wefkjn"
+
+// prefer not to var due to its block scope
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountCity, accountPassword, accountState])
