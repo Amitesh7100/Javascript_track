dfbc666670d3dac1baf6ba375034c5f228004070
Amitesh7100 <110113872+Amitesh7100@users.noreply.github.com>
2024-12-11 17:42:40 +0000

    done

---
+++ learn/datatypes-summary.js
@@ -0,0 +1,31 @@
+// Primitive
+//7 types: String,Num , Boolean, Null, Undefined, Symbol, BigInt
+const score=100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp =null
+let userEmail;
+
+const id= Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id === anotherId);
+
+//const bigNumber = 36594965156n
+
+//Reference(Non premitive)
+
+// Arrays, Objects, Functions
+
+const heros =["ironman", "naljbg","jhfbckma"]
+
+let myobj={
+    name:"ekjfhb",
+    age: 22,
+}
+
+const myFunction = function() {
+    console.log("heloooo");
+}
+console.log(typeof outsideTemp);
