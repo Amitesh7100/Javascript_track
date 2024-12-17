b1d03c84bf444ea953b431ff734d98d00a414100
Amitesh7100 <110113872+Amitesh7100@users.noreply.github.com>
2024-12-17 18:11:16 +0000

    done

---
+++ controlflow/one.js
@@ -0,0 +1,19 @@
+ const balance = 500
+// if(balance > 500) console.log("test")
+
+
+
+// if(balance <500) {
+//     console.log("less than 500");
+// } else if(balance < 750){
+//     console.log("less than 750");
+// }else {
+//     console.log("less than 1200");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+if(userLoggedIn && debitCard){
+    console.log("allow to buy product");
+}
