16b9bf6d02b8225477dc7f0b8c53c6a5db9f6f1a
Amitesh7100 <110113872+Amitesh7100@users.noreply.github.com>
2024-12-19 17:21:04 +0000

    done

---
+++ iterations/two.js
@@ -0,0 +1,14 @@
+const myObject = {
+    js: 'javascript',
+    py: 'python',
+    cpp: 'c++'
+}
+for (const key in myObject) {
+    console.log(myObject[key]);
+}
+
+const programming =["js","rb", "py", "java","cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
